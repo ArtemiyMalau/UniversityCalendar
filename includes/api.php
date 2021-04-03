dfc9f0621de5cc986271f87af7af9a5ec00f8a74
Artemiy <arsenij.blokov@gmail.com>
2021-04-03 17:01:54 +0300

    Refactoring API

--- includes/api.php
+++ includes/api.php
@@ -9,58 +9,42 @@ use Krugozor\Database\MySqlException;
 
 session_start();
 
-
 $api = new SimpleAPI();
 
 switch ($api->module) {
 	case "get_day_schedules":
 		$data = $api->params(["date"]);
 
-		$dt = new DateTime();
-		$midnight = $dt->setTimestamp($data["date"])->setTime(0, 0)->getTimestamp();
-		$end_day = $midnight + 86399;
-
-		$schedules = $db->query("SELECT schedule.id, schedule.date, subjects.title FROM schedule
-			JOIN subjects ON schedule.id_subject = subjects.id
-			WHERE schedule.date BETWEEN ?i AND ?i
-			ORDER BY schedule.date", $midnight, $end_day)->fetchAssocArray();
-
+		$schedules = $db->query("SELECT sch.id, sch.date, sub.title, spec.* 
+			FROM schedule sch
+			JOIN subjects sub ON sch.id_subject = sub.id
+			JOIN subjects_to_specialities sub_to_spec ON sub.id = sub_to_spec.id_subject
+			JOIN specialities spec ON sub_to_spec.id_speciality = spec.id
+			WHERE DATE(sch.date) = DATE(FROM_UNIXTIME(?i))
+			ORDER BY sch.date", $data["date"])->fetchAssocArray();
+		
 		$api->answer["schedules"] = $schedules;
 
-	case "get_interval_schedule_counts":
+	case "get_interval_schedules":
 		$data = $api->params(["start_date", "end_date"]);
 
-		$sec_per_day = 86400;
+		$events = $db->query("SELECT COUNT(*) as count, UNIX_TIMESTAMP(schedule.date) as 'timestamp' 
+			FROM schedule
+			WHERE DATE(schedule.date) BETWEEN DATE(FROM_UNIXTIME(?i)) AND DATE(FROM_UNIXTIME(?i))
+			GROUP BY DATE(schedule.date)
+			ORDER BY schedule.date", $data["start_date"], $data["end_date"])->fetchAssocArray();
 
-		$dt = new DateTime();
-		$start_date_stamp = $dt->setTimestamp($data["start_date"])->setTime(0, 0)->getTimestamp();
-		$end_date_stamp = $dt->setTimestamp($data["end_date"])->setTime(23, 59, 59)->getTimestamp();
-
-		$events = $db->query("SELECT schedule.id, schedule.date, subjects.title FROM schedule
-			JOIN subjects ON schedule.id_subject = subjects.id
-			WHERE schedule.date BETWEEN ?i AND ?i
-			ORDER BY schedule.date", $start_date_stamp, $end_date_stamp)->fetchAssocArray();
-
-		$cur_date_stamp = $start_date_stamp - $sec_per_day;
-		$schedule_counts = [];
-		foreach ($events as $event) {
-			while ($event["date"] >= $cur_date_stamp + $sec_per_day) {
-				$cur_date_stamp += $sec_per_day;
-				$schedule_counts[] = ["start_date" => $cur_date_stamp, "end_date" => $cur_date_stamp + $sec_per_day, "count" => 0];
-			}
-			$schedule_counts[count($schedule_counts)-1]["count"] += 1;
-		}
-
-		$api->answer["schedule_counts"] = $schedule_counts;
+		$api->answer["interval_schedules"] = $events;
 
 	case "add_schedule":
 		$data = $api->params(["id_subject", "date"]);
 
 		try {
-			$db->query("INSERT INTO schedule(id_subject, date) VALUES (?i, ?i)", $data["id_subject"], $data["date"]);
+			$db->query("INSERT INTO schedule(id_subject, date) VALUES (?i, FROM_UNIXTIME(?i))", $data["id_subject"], $data["date"]);
 
 			$api->answer["status"] = true;
 		} catch (MySqlException $e) {
 			$api->answer["status"] = false;
 		}
 }
+
