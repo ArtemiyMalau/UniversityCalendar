97243afe57276a7939e9cd4a1306adc93dc68775
Artemiy <arsenij.blokov@gmail.com>
2021-04-04 00:01:04 +0300

    Fix API

--- includes/api.php
+++ includes/api.php
@@ -15,20 +15,39 @@ switch ($api->module) {
 	case "get_day_schedules":
 		$data = $api->params(["date"]);
 
-		$schedules = $db->query("SELECT sch.id, sch.date, sub.title, spec.* 
-			FROM schedule sch
-			JOIN subjects sub ON sch.id_subject = sub.id
-			JOIN subjects_to_specialities sub_to_spec ON sub.id = sub_to_spec.id_subject
-			JOIN specialities spec ON sub_to_spec.id_speciality = spec.id
-			WHERE DATE(sch.date) = DATE(FROM_UNIXTIME(?i))
-			ORDER BY sch.date", $data["date"])->fetchAssocArray();
+		$resp = $db->query("SELECT sch.date, sub.title AS sub_title, spec.*, 
+			DENSE_RANK() OVER (ORDER BY sch.date, sub.title) AS idx 
+			FROM schedule sch 
+			JOIN subjects sub ON sch.id_subject = sub.id 
+			JOIN subjects_to_specialities sub_to_spec ON sub.id = sub_to_spec.id_subject 
+			JOIN specialities spec ON sub_to_spec.id_speciality = spec.id 
+			WHERE DATE(sch.date) = DATE(FROM_UNIXTIME(?i)) 
+			ORDER BY sch.date, idx", $data["date"])->fetchAssocArray();
 		
+		$schedules = [];
+		$idx = null;
+		foreach ($resp as $schedule) {
+			if ($idx != $schedule["idx"]) {
+				$idx = $schedule["idx"];
+				$schedules[] = [
+					"date" => $schedule["date"], 
+					"title" => $schedule["sub_title"], 
+					"specialities" => []
+				];
+			}
+			$schedules[count($schedules) - 1]["specialities"][] = [
+				"code" => $schedule["code"], 
+				"title" => $schedule["title"]
+			];
+		}
+
+
 		$api->answer["schedules"] = $schedules;
 
 	case "get_interval_schedules":
 		$data = $api->params(["start_date", "end_date"]);
 
-		$events = $db->query("SELECT COUNT(*) as count, UNIX_TIMESTAMP(schedule.date) as 'timestamp' 
+		$events = $db->query("SELECT COUNT(*) AS count, UNIX_TIMESTAMP(schedule.date) AS 'timestamp' 
 			FROM schedule
 			WHERE DATE(schedule.date) BETWEEN DATE(FROM_UNIXTIME(?i)) AND DATE(FROM_UNIXTIME(?i))
 			GROUP BY DATE(schedule.date)
