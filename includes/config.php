f06333862e02af0cacdb639e2a3450dbb21e4851
Artemiy <arsenij.blokov@gmail.com>
2021-04-05 10:00:47 +0300

    Add config

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array(
+    "db" => array(
+        "host" => "%host%",
+        "user" => "%user%",
+        "password" => "%password%",
+        "database" => "%database%"
+    ),
+
+    "fs" => array(
+    	"api" => "%project_url%/includes/api.php",
+    	"tpl" => "%project_path%/tpl"
+    )
+);
